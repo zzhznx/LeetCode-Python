ceb9afc25ec4df678cbe882b1d8102288c9fcaf5
zhangzhenhua <zzhznx@163.com>
2016-05-04 22:20:23 +0800

    nice

--- 48-Rotate Image.py
+++ 48-Rotate Image.py
@@ -1,4 +1,12 @@
 class Solution(object):
+
+    '''
+    clockwise rotate
+    first reverse up to down, then swap the symmetry
+    1 2 3     7 8 9     7 4 1
+    4 5 6  => 4 5 6  => 8 5 2
+    7 8 9     1 2 3     9 6 3
+    '''
     def rotate(self, matrix):
         """
         :type matrix: List[List[int]]
@@ -10,6 +18,13 @@ class Solution(object):
                 (matrix[i][j], matrix[j][i]) = (matrix[j][i], matrix[i][j])
         return matrix
 
+    '''
+    anticlockwise rotate
+    first reverse left to right, then swap the symmetry
+    1 2 3     3 2 1     3 6 9
+    4 5 6  => 6 5 4  => 2 5 8
+    7 8 9     9 8 7     1 4 7
+    '''
     def anti_rotate(self, matrix):
         for m in matrix:
             m[:] = m[::-1]
