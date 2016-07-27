801e61cd76ed0f3629c718c581843c150282fa8e
zhangzhenhua <zzhznx@163.com>
2016-07-27 20:50:15 +0800

    nice

---
+++ 50-Pow(x, n).py
@@ -0,0 +1,17 @@
+class Solution(object):
+    def myPow(self, x, n):
+        """
+        :type x: float
+        :type n: int
+        :rtype: float
+        """
+        if not n:
+            return 1
+        if n < 0:
+            return 1 / self.myPow(x, -n)
+        if n % 2:
+            return x * self.myPow(x, n - 1)
+        return self.myPow(x * x, n / 2)
+
+solution = Solution()
+print(solution.myPow(3, 4))
