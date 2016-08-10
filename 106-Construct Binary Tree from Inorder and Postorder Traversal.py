9d92e1c6d0d6c804e52c07ef46aeba3a056913ef
zhangzhenhua <zzhznx@163.com>
2016-08-10 17:38:18 +0800

    nice

--- 106-Construct Binary Tree from Inorder and Postorder Traversal.py
+++ 106-Construct Binary Tree from Inorder and Postorder Traversal.py
@@ -16,8 +16,24 @@ class Solution(object):
             return root
 
     def buildTree2(self, inorder, postorder):
+        parents = []
+        working = None
+        i, post = 0, 0
+        while post < len(postorder):
+            if len(parents) > 0 and postorder[post] == parents[-1].val:
+                parents[-1].right = working
+                working = parents[-1]
+                parents.pop()
+                post += 1
+            else:
+                newNode = tree.TreeNode(inorder[i])
+                newNode.left = working
+                working = None
+                parents.append(newNode)
+                i += 1
+        return working
 
 
 solution = Solution()
-newRoot = solution.buildTree([4, 2, 7, 5, 8, 1, 3, 6], [4, 7, 8, 5, 2, 6, 3, 1])
+newRoot = solution.buildTree2([4, 2, 7, 5, 8, 1, 3, 6], [4, 7, 8, 5, 2, 6, 3, 1])
 tree.pretty_print(newRoot)
