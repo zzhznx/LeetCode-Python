528f267dbba1640436f376512f09bf829b5739a7
zhangzhenhua <zzhznx@163.com>
2016-08-11 21:14:25 +0800

    nice

--- 257-Binary Tree Paths.py
+++ 257-Binary Tree Paths.py
@@ -18,7 +18,41 @@ class Solution:
             if root.left is None and root.right is None:
                 res.append(ls + str(root.val))
 
+    # dfs + stack
+    def binaryTreePaths2(self, root):
+        if not root:
+            return []
+        stack, res = [(root, str(root.val))], []
+        while stack:
+            node, ls = stack.pop()
+            if node:
+                if not node.left and not node.right:
+                    res.append(ls)
+                if node.right:
+                    stack.append((node.right, ls + "->" + str(node.right.val)))
+                if node.left:
+                    stack.append((node.left, ls + "->" + str(node.left.val)))
+        return res
+
+    # bfs + queue
+    def binaryTreePaths3(self, root):
+        if not root:
+            return []
+        q, res = queue.Queue(), []
+        q.put((root, str(root.val)))
+        while not q.empty():
+            node, ls = q.get()
+            if node:
+                if not node.left and not node.right:
+                    res.append(ls)
+                if node.left:
+                    q.put((node.left, ls + "->" + str(node.left.val)))
+                if node.right:
+                    q.put((node.right, ls + "->" + str(node.right.val)))
+        return res
 
 root = tree.get_one()
 solution = Solution()
 print(solution.binaryTreePaths(root))
+print(solution.binaryTreePaths2(root))
+print(solution.binaryTreePaths3(root))
--- Tree_zzh/tree.py
+++ Tree_zzh/tree.py
@@ -1,4 +1,5 @@
 import queue
+from io import StringIO
 
 class TreeNode(object):
     def __init__(self, x):
@@ -26,7 +27,7 @@ def get_one():
     return nodeList[1]
 
 
-def pretty_print(root):
+def level_print(root):
     if root is None:
         return []
     q = queue.Queue()
@@ -44,3 +45,92 @@ def pretty_print(root):
                 q.put(node.right)
         res.append(temp)
     print(res)
+
+
+class Queue(object):
+    def __init__(self):
+        self.queue = []
+
+    def enqueue(self, b):
+        self.queue.insert(0, b)
+
+    def dequeue(self):
+        return self.queue.pop()
+
+    def isEmpty(self):
+        return self.queue == []
+
+
+def add_padding(str, pad_length_value):
+    str = str.strip()
+    return str.center(pad_length_value, ' ')
+
+
+def pretty_print(root):
+    output = StringIO()
+    pretty_output = StringIO()
+    current_level = Queue()
+    next_level = Queue()
+    current_level.enqueue(root)
+    depth = 0
+
+    # get the depth of current tree
+    # get the tree node data and store in list
+    if root:
+        while not current_level.isEmpty():
+            current_node = current_level.dequeue()
+            output.write('%s ' % current_node.val if current_node else 'N ')
+            next_level.enqueue(current_node.left if current_node else current_node)
+            next_level.enqueue(current_node.right if current_node else current_node)
+            if current_level.isEmpty():
+                if sum([i is not None for i in next_level.queue]):  # if next level has node
+                    current_level, next_level = next_level, current_level
+                    depth += 1
+                output.write('\n')
+    print('the tree print level by level is :')
+    print(output.getvalue())
+    print("current tree's depth is %i" % (depth + 1))
+
+    # add space to each node
+    output.seek(0)
+    pad_length = 3
+    keys = []
+    spaces = 2 ** depth
+    while spaces > 0:
+        skip_start = spaces * pad_length
+        skip_mid = (2 * spaces - 1) * pad_length
+        key_start_spacing = ' ' * skip_start
+        key_mid_spacing = ' ' * skip_mid
+        keys = output.readline().split(' ')  # read one level to parse
+        padded_keys = (add_padding(key, pad_length) for key in keys)
+        padded_str = key_mid_spacing.join(padded_keys)
+        complete_str = ''.join([key_start_spacing, padded_str])
+        pretty_output.write(complete_str)
+
+        # add space and slashes to middle layer
+        slashes_depth = spaces
+        print('current slashes depth im_resize:')
+        print(spaces)
+        print("current levle's list is:")
+        print(keys)
+
+        spaces //= 2
+        if spaces > 0:
+            pretty_output.write('\n')  # print '\n' each level
+
+            cnt = 0
+            while cnt < slashes_depth:
+                inter_symbol_spacing = ' ' * (pad_length + 2 * cnt)
+                symbol = ''.join(['/', inter_symbol_spacing, '\\'])
+                symbol_start_spacing = ' ' * (skip_start - cnt - 1)
+                symbol_mid_spacing = ' ' * (skip_mid - 2 * (cnt + 1))
+                pretty_output.write(''.join([symbol_start_spacing, symbol]))
+                for i in keys[1:-1]:
+                    pretty_output.write(''.join([symbol_mid_spacing, symbol]))
+                pretty_output.write('\n')
+                cnt += 1
+    print(pretty_output.getvalue())
+
+
+root = get_one()
+pretty_print(root)
