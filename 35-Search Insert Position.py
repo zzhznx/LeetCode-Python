8d32df963e1203398e900762e674c097a537a8fc
zhangzhenhua <zzhznx@163.com>
2016-08-29 21:59:40 +0800

    nice

--- 35-Search Insert Position.py
+++ 35-Search Insert Position.py
@@ -10,20 +10,29 @@ class Solution(object):
         if target < nums[0]:
             return 0
 
-        l, r = 0, len(nums) - 1
-        while l <= r:
-            m = int((l + r) / 2)
-            print("m", m)
-            if nums[m] > target:
-                r = m - 1
-                if nums[r] < target:
-                    return r + 1
-            elif nums[m] < target:
-                l = m + 1
-                if nums[l] > target:
-                    return l
+        # l, r = 0, len(nums) - 1
+        # while l <= r:
+        #     m = int((l + r) / 2)
+        #     if nums[m] > target:
+        #         r = m - 1
+        #         if nums[r] < target:
+        #             return r + 1
+        #     elif nums[m] < target:
+        #         l = m + 1
+        #         if nums[l] > target:
+        #             return l
+        #     else:
+        #         return m
+
+        low, high, ans = 0, len(nums) - 1, -1
+        while low <= high:
+            mid = (low + high) // 2
+            if nums[mid] < target:
+                low = mid + 1
             else:
-                return m
+                high = mid - 1
+                ans = mid
+        return ans
 
 solution = Solution()
 print(solution.searchInsert([1, 3, 5], 5))
